8d0f93345aaed657522175f71548733054a89d18
niassuh1 <galenwaros@gmail.com>
2021-11-19 20:27:57 +0300

    Added entities

---
+++ lib/entities/doctor.dart
@@ -0,0 +1,11 @@
+class Doctor {
+  final String firstName;
+  final String lastName;
+  final String specialization;
+  final double rating;
+  const Doctor(
+      {required this.firstName,
+      required this.lastName,
+      required this.rating,
+      required this.specialization});
+}
